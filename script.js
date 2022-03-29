b195d962caf3e46b6b4e356587ebbd6e97b85473
Stella <biliou.stella@gmail.com>
2022-03-29 09:55:26 +0200

    Added button with function that can alternate between two options

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var count = 0;
 
+function getSurvey() {
+    if(count % 2 == 0) {
+        count++;
+        alert("open form 1");
+        //window.open("https://forms.gle/imgzGaRM9836X9AH8");        
+    }
+    else {
+        count++;
+        alert("open form 2");
+    }
+}
